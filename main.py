77e447f63f5c9dccb82a238b644aff5e722d9ce0
Mark N. Hernandez <marknhernandez@icloud.com>
2020-08-01 15:33:52 -0500

    basic functionality created

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
 from tkinter import *
 
+#canvas settings
 root = Tk()
 root.geometry("800x800")
+
 #variables
 board_width = 530
 board_height = 600
 r = 65
 img = PhotoImage(file="redDot.gif")
+button = []
+col = [6,6,6,6,6,6,6]
+turn = 1
 
-#change the color of the dot
-def changeColor():
-    board.itemconfig(1, fill='yellow')
-    board.itemconfig(2, fill='yellow')
-    board.itemconfig(3, fill='yellow')
-    board.itemconfig(4, fill='yellow')
-    board.itemconfig(5, fill='yellow')
-    board.itemconfig(11, fill='yellow')
-    board.itemconfig(12, fill='yellow')
-    board.itemconfig(13, fill='yellow')
-    board.itemconfig(14, fill='yellow')
-    board.itemconfig(15, fill='yellow')
-
-#create the grid of circles
-def createGrid():
-    for i in range(1,8,1):
-        for j in range(1,7,1):
-            dot = board.create_oval(75*i, 75*j, (75*i)-r, (75*j)-r, fill='black')
-
-
+#create the board
 board = Canvas(width=board_width, height=board_height, bg='blue')
 board.pack()
 board.place(x=135, y=61)
+
+#create the grid of circles
+def createGrid():
+    for i in range(7):
+        for j in range(6):
+            board.create_oval(75*(i+1), 75*(j+1), (75*(i+1)-r), (75*(j+1)-r), fill='black')
+
+#create the array of buttons and place on the canvas
+def createButtons():
+    for i in range(7):
+        button.append(Button(root, command=lambda c=i: changeColor(c), image=img, width=60))
+        button[i].pack()
+        button[i].place(x=(145+(75*i)), y=0)
+
+#change the color of the dot
+def changeColor(c):
+    #place the coin
+    if c == 0:
+        board.itemconfig((c*6)+col[0], fill='red')
+        col[0] -=1
+        print(col[0])
+    elif c == 1:
+        board.itemconfig((c*6)+col[1], fill='yellow')
+        col[1] -=1
+        print(col[1])
+    elif c == 2:
+        board.itemconfig((c*6)+col[2], fill='yellow')
+        col[2] -=1
+        print(col[2])
+    elif c == 3:
+        board.itemconfig((c*6)+col[3], fill='yellow')
+        col[3] -=1
+        print(col[3])
+    elif c == 4:
+        board.itemconfig((c*6)+col[4], fill='yellow')
+        col[4] -=1
+        print(col[4])
+    elif c == 5:
+        board.itemconfig((c*6)+col[5], fill='yellow')
+        col[5] -=1
+        print(col[5])
+    elif c == 6:
+        board.itemconfig((c*6)+col[6], fill='yellow')
+        col[6] -=1
+        print(col[6])
+  
+
 createGrid()
-B = Button(root, command=changeColor, image=img, width=60)
-B.pack()
-B.place(x=145, y=0)
+createButtons()
 root.mainloop()
