6d0a657f79e8a78bc54bc4c33fc00cadbfaf4642
Mark N. Hernandez <marknhernandez@icloud.com>
2020-08-02 18:23:45 -0500

    added yellow dot and reset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import numpy as np
 
 #canvas settings
 root = Tk()
@@ -8,10 +9,12 @@ root.geometry("800x800")
 board_width = 530
 board_height = 600
 r = 65
-img = PhotoImage(file="redDot.gif")
+imgR = PhotoImage(file="redDot.gif")
+imgY = PhotoImage(file="yellowDot.gif")
+imgRe = PhotoImage(file="reset.gif")
 button = []
 col = [6,6,6,6,6,6,6]
-#turn 0: red turn 1: yellow
+#turn True: red; turn False: yellow
 turn = True
 
 #create the board
@@ -19,17 +22,24 @@ board = Canvas(width=board_width, height=board_height, bg='blue')
 board.pack()
 board.place(x=135, y=61)
 
-##DON'T JUDGE THIS, IT'S A WORK IN PROGRESS
-
+def changeButtons():
+    global turn
+    if turn == True:
+        for i in range(len(button)):
+            button[i]['image'] = imgY
+    else:
+        for i in range(len(button)):
+            button[i]['image'] = imgR
+    
 # def checkWinner(index):
-#     #back and to the left
-#     conditions = [[0, -7, -14, -21], [0, -1, -2, -3], [0, 5, 10, 15], [0, 6, 12, 18], [0, 7, 14, 21], [0, 1, 2, 3], [0, -5, -10, -15], [0, -6, -12, -18]]
-#     for i in range(len(conditions)):
-#         for j in range(len(conditions[i])):
-#             if any(board.itemcget(index+j, "fill") == 'red'):
+#     global turn
+#     buffer =[]
+#     conditions = np.array([[0, -7, -14, -21], [0, -1, -2, -3], [0, 5, 10, 15], [0, 6, 12, 18], [0, 7, 14, 21], [0, 1, 2, 3], [0, -5, -10, -15], [0, -6, -12, -18]])
+#     buffer = conditions + index
+#     for i in range(len(buffer)):
+#         for j in range(len(buffer[i])):
+#             if all(board.itemcget(buffer[i][j], "fill") == 'red'):
 #                 print('winner')
-#     color = board.itemcget(index, "fill")
-#     print(color)
 
 def turnSelect():
     global turn
@@ -52,6 +62,7 @@ def resetGrid():
     global button
     for i in range(len(button)):
         button[i]['state'] = 'normal'
+        button[i]['image'] = imgR
     for i in range(1,43,1):
         board.itemconfig(i, fill='black')
         col = [6,6,6,6,6,6,6]
@@ -61,7 +72,7 @@ def resetGrid():
 #create the array of buttons and place on the canvas
 def createButtons():
     for i in range(7):
-        button.append(Button(root, command=lambda c=i: changeColor(c), image=img, width=60))
+        button.append(Button(root, command=lambda c=i: changeColor(c), image=imgR, width=60))
         button[i].pack()
         button[i].place(x=(145+(75*i)), y=0)
 
@@ -70,40 +81,34 @@ def changeColor(c):
     #place the coin
     global col
     global button
+    changeButtons()
     if c == 0:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[0] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(((c*6)+col[c]))
     elif c == 1:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[1] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[1])
     elif c == 2:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[2] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[2])
     elif c == 3:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[3] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[3])
     elif c == 4:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[4] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[4])
     elif c == 5:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[5] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[5])
     elif c == 6:
         board.itemconfig((c*6)+col[c], fill=turnSelect())
         col[6] -=1
-        #checkWinner(((c*6)+col[c])+1)
         print(col[6])
     if any(t == 0 for t in col):
         col[c] = 6
@@ -113,7 +118,7 @@ def changeColor(c):
 
 createGrid()
 createButtons()
-reset = Button(root, command=resetGrid, image=img, width=60)
+reset = Button(root, command=resetGrid, image=imgRe, width=240)
 reset.pack()
-reset.place(x=370, y=700)
+reset.place(x=270, y=700)
 root.mainloop()
