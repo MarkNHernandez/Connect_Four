cd8e60689ee3bd5f580ca78b4e4c389fa5a427a5
Mark N Hernandez <marknhernandez@icloud.com>
2023-05-25 14:12:42 -0500

    WIP commit

--- main.py
+++ main.py
@@ -2,6 +2,27 @@ from tkinter import *
 from tkinter import messagebox as mb
 from enum import Enum
 
+#region Properties
+screen_width = 800
+screen_height = 800
+board_width = 530
+board_height = 600
+board_position_x = 0
+board_position_y = 65
+dot_diameter = 75
+dot_radius = 65
+number_of_rows = 6
+number_of_columns = 7
+
+#endregion
+
+#canvas settings
+root = Tk()
+root.geometry("{}x{}".format(screen_width, screen_height))
+image_red_dot = PhotoImage(file="redDot.gif")
+image_yellow_dot = PhotoImage(file="yellowDot.gif")
+image_reset = PhotoImage(file="reset.gif")
+#create the board
 
 #region Classes
 
@@ -10,119 +31,55 @@ class State(Enum):
     player_one = 1
     player_two = 2
 
-class Game():
-    def __init__(self):
-        self.turn = State.player_one
-        self.winner = False
-
 class Color(str, Enum):
     BLACK = "black"
     RED = "red"
     YELLOW = "yellow"
 
+class GameButton(object):
+    def __init__(self, column, image, width):
+        self.image = image_red_dot
+        self.button = Button(root, command = self.change_color, image = image, width = width)
+        self.button.grid(row = 0, column=column, padx = 5)
+
+    def change_color(self):
+        pass
+
 class Dot:
-    def __init__(self, row, column, diameter, radius):
+    def __init__(self, board, row, column, diameter, radius):
         self.row = row
         self.column = column
         self.fill = Color.BLACK
         self.state = State.empty
-        board.create_oval(diameter*(row), 
-                          diameter*(column), 
-                          (diameter*(row)-radius), 
-                          (diameter*(column)-radius), 
-                          fill = Color.BLACK)
+        self.oval = board.create_oval(diameter*(row), 
+                                      diameter*(column),
+                                      (diameter*(row)-radius),
+                                      (diameter*(column)-radius),
+                                      fill = self.fill)
     def reset(self):
-        fill = Color.BLACK
-        state = State.empty
+        self.oval.fill = Color.BLACK
+        self.state = State.empty
 
-class Game_Button:
-    def __init__(self, column):
-       self.column = column
-       Button(root, 
-              command = change_color(), 
-              image=image_red_dot, 
-              width=60) 
+class Board(object):
+    def __init__(self, width, height, x_placement, y_placement, background_color):
+        self.board = Canvas(width=width, height=height, bg=background_color)
+        self.board.pack()
+        self.board.place(x=x_placement, y=y_placement)
     
+        self.dots = []
+        for i in range(1, number_of_columns+1):
+            for j in range(1, number_of_rows+1):
+                self.dots.append(Dot(self.board, i, j, dot_diameter, dot_radius))
 
+        self.buttons = []
+        for i in range(number_of_columns):
+            self.buttons.append(GameButton(i, image_red_dot, 60, self.dots))
+
+        
 #endregion
-
-
-#region Properties
-
-screen_width = 800
-screen_height = 800
-board_width = 530
-board_height = 600
-dot_diameter = 75
-dot_radius = 65
-buttons = []
-dots = []
-col = [6,6,6,6,6,6,6]
-turn = True
-
-#endregion
-
-#canvas settings
-root = Tk()
-root.geometry("800x800")
-image_red_dot = PhotoImage(file="redDot.gif")
-image_yellow_dot = PhotoImage(file="yellowDot.gif")
-image_reset = PhotoImage(file="reset.gif")
-#create the board
-board = Canvas(width=board_width, height=board_height, bg='blue')
-board.pack()
-board.place(x=135, y=61)
- 
-def turn_select():
-    global turn
-    if turn == True:
-        turn = False
-        return str('red')
-    elif turn == False:
-        turn = True
-        return str('yellow')
-
-#create the grid of circles
-def create_grid():
-    for i in range(1,7):
-        for j in range(1,8):
-            dots.append(Dot(j, i, dot_diameter, dot_radius))
-
-def reset_grid():
-    for dot in dots:
-        dot.reset()
-
-#create the array of buttons and place on the canvas
-def create_buttons():
-    for i in range(7):
-        buttons.append()
-        buttons[i].pack()
-        buttons[i].place(x=(145+(dot_diameter*i)), y=0)
-
-#change the color of the dot
-def change_color(c):
-    #place the coin
-    global col
-    global button
-    index = (c*6)+col[c]
-    change_buttons()
-
-    board.itemconfig(index, fill=turn_select())
-    col[c] -=1
-    print(col[c])
-
-    if any(t == 0 for t in col):
-        col[c] = 6
-        buttons[c]['state'] = 'disabled'
-    
-    check_winner(index)
 
 def main():
-    create_grid()
-    create_buttons()
-    reset = Button(root, command=reset_grid, image=image_reset, width=240)
-    reset.pack()
-    reset.place(x=270, y=700)
+    game = Board(board_width, board_height, board_position_x, board_position_y, "blue")
     root.mainloop()
 
 if __name__ == "__main__":
